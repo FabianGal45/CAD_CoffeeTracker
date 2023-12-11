38fef49c6dc46e6fbeab57c572492b4818fc603a
FabianGal45 <fabiangal.45@gmail.com>
2023-12-11 13:36:20 +0000

    Added Unit testing

--- test/models/coffee_purchase_test.rb
+++ test/models/coffee_purchase_test.rb
@@ -2,10 +2,31 @@ require "test_helper"
 
 class CoffeePurchaseTest < ActiveSupport::TestCase
   # Unit testing
-
+  setup do
+    CoffeePurchase.delete_all # this clears any existing values 
+  end
   # test "the truth" do
   #   assert true
   # end
 
+  #Test adding price 10 in week 1 of month 1 - should display 10
+  test "should add a purchase" do
+    CoffeePurchase.create(price: 10, purchased_at: Time.now) # Week 1, Monday
+    assert_equal(10, CoffeePurchase.weekly_spending)
+  end
+
+  #Test adding price 15 in week 2 of month 1 - should display 15
+  test "should display the current week price" do
+    CoffeePurchase.create(price: 10, purchased_at: Time.now.prev_week.beginning_of_week) # A week before
+    CoffeePurchase.create(price: 15, purchased_at: Time.now) # This week
+    assert_equal(15, CoffeePurchase.weekly_spending)
+  end
+  #Test adding price 10 in week 1, adding price 15 in week 2 and display monthly value of 25
+  test "should display both prices of the month for both weeks" do
+    
+    CoffeePurchase.create(price: 10, purchased_at: Time.now.prev_week.beginning_of_week) # A week before
+    CoffeePurchase.create(price: 15, purchased_at: Time.now) # This week
+    assert_equal(25, CoffeePurchase.monthly_spending.to_f)
+  end
   
 end
