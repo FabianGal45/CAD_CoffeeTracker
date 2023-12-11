af29b176168f421d421d336e3fee60fb5a6319f8
FabianGal45 <fabiangal.45@gmail.com>
2023-12-11 01:34:45 +0000

    another edit

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,12 @@
 CURRENT_INSTANCE=$(sudo docker ps -q --filter ancestor="$IMAGE_NAME")
 if [ "$CURRENT_INSTANCE" ]; then
   sudo docker stop $CURRENT_INSTANCE
+  sudo docker rm $CURRENT_INSTANCE
 fi
 
 # Pull down the new image
 sudo docker pull $IMAGE_NAME
 
-# Remove the stopped container
-if [ "$CURRENT_INSTANCE" ]; then
-  sudo docker rm $CURRENT_INSTANCE
-fi
 
 # Run the new image
 sudo docker run -e RAILS_ENV=$production  -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -e SECRET_KEY_BASE=$SECRET_KEY_BASE -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME bin/rails server -b 0.0.0.0 
